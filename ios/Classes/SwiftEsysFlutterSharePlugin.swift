4b9d8b2e6b133e16a41ffd4556c30f1e2c947318
sharansingh00002 <sharansingh00002@gmail.com>
2019-08-09 13:52:13 +0530

    removed not wanted code

--- ios/Classes/SwiftEsysFlutterSharePlugin.swift
+++ ios/Classes/SwiftEsysFlutterSharePlugin.swift
@@ -10,34 +10,10 @@ public class SwiftEsysFlutterSharePlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        if(call.method == "text"){
-            self.text(arguments: call.arguments)
-        }
         if(call.method == "file"){
             self.file(arguments: call.arguments)
         }
-        if(call.method == "files"){
-            self.files(arguments: call.arguments)
-        }
     }
-    
-    func text(arguments:Any?) -> Void {
-        // prepare method channel args
-        // no use in ios
-        //// let title:String = argsMap.value(forKey: "title") as! String
-        let argsMap = arguments as! NSDictionary
-        let text:String = argsMap.value(forKey: "text") as! String
-        
-        // set up activity view controller
-        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: [text], applicationActivities: nil)
-        
-        // present the view controller
-        let controller = UIApplication.shared.keyWindow!.rootViewController as! FlutterViewController
-        activityViewController.popoverPresentationController?.sourceView = controller.view
-        
-        controller.show(activityViewController, sender: self)
-    }
-    
     func file(arguments:Any?) -> Void {
         // prepare method channel args
         // no use in ios
@@ -52,38 +28,6 @@ public class SwiftEsysFlutterSharePlugin: NSObject, FlutterPlugin {
         
         // prepare sctivity items
         var activityItems:[Any] = [contentUri!];
-        if(!text.isEmpty){
-            // add optional text
-            activityItems.append(text);
-        }
-        
-        // set up activity view controller
-        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
-        
-        // present the view controller
-        let controller = UIApplication.shared.keyWindow!.rootViewController as! FlutterViewController
-        activityViewController.popoverPresentationController?.sourceView = controller.view
-        
-        controller.show(activityViewController, sender: self)
-    }
-    
-    func files(arguments:Any?) -> Void {
-        // prepare method channel args
-        // no use in ios
-        //// let title:String = argsMap.value(forKey: "title") as! String
-        let argsMap = arguments as! NSDictionary
-        let names:[String] = argsMap.value(forKey: "names") as! [String]
-        let text:String = argsMap.value(forKey: "text") as! String
-        
-        // prepare sctivity items
-        var activityItems:[Any] = [];
-        
-        // load the files
-        for name in names {
-            let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!;
-            activityItems.append(NSURL(fileURLWithPath: docsPath).appendingPathComponent(name)!);
-        }
-        
         if(!text.isEmpty){
             // add optional text
             activityItems.append(text);
--- lib/esys_flutter_share.dart
+++ lib/esys_flutter_share.dart
@@ -8,19 +8,10 @@ class Share {
   static const MethodChannel _channel = const MethodChannel(
       'channel:github.com/orgs/esysberlin/esys-flutter-share');
 
-  /// Sends a text to other apps.
-  static void text(String title, String text, String mimeType) {
-    Map argsMap = <String, String>{
-      'title': '$title',
-      'text': '$text',
-      'mimeType': '$mimeType'
-    };
-    _channel.invokeMethod('text', argsMap);
-  }
-
   /// Sends a file to other apps.
   static Future<void> file(
-      String title, String name, List<int> bytes, String mimeType, {String text = ''}) async {
+      String title, String name, List<int> bytes, String mimeType,
+      {String text = ''}) async {
     Map argsMap = <String, String>{
       'title': '$title',
       'name': '$name',
@@ -33,25 +24,5 @@ class Share {
     await file.writeAsBytes(bytes);
 
     _channel.invokeMethod('file', argsMap);
-  }
-
-  /// Sends multiple files to other apps.
-  static Future<void> files(
-      String title, Map<String, List<int>> files, String mimeType, {String text = ''}) async {
-    Map argsMap = <String, dynamic>{
-      'title': '$title',
-      'names': files.entries.toList().map((x) => x.key).toList(),
-      'mimeType': mimeType,
-      'text': '$text'
-    };
-
-    final tempDir = await getTemporaryDirectory();
-
-    for (var entry in files.entries) {
-      final file = await new File('${tempDir.path}/${entry.key}').create();
-      await file.writeAsBytes(entry.value);
-    }
-
-    _channel.invokeMethod('files', argsMap);
   }
 }
